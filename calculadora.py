9ea86786cad65629344637c795db40f9abf01609
Junior Elias <junior.elias.souza17@gmail.com>
2024-02-20 18:30:22 -0300

    calculadora

--- calculadora.py
+++ calculadora.py
@@ -10,8 +10,8 @@ print('''Segue abaixo as opções para utilizar:
       [2] Subtrair.
       [3] Divisão.
       [4] Multiplicação.
-      [5] Menor e maior.
-      [6] Tabuada.
+      [5] Tabuada.
+      [6] Menor e maior.
       [7] Sair.''')
 opc = int(input('''Digite a opção na qual você deseja: '''))
 while True:
@@ -24,4 +24,41 @@ while True:
         res = n1+n2
         print('{:.2f} + {:.2f} = {:.2f}'.format(n1, n2, res))
     elif opc == 2:
-        n1 = float(input('Digite um número'))
+        n1 = float(input('Digite um número: '))
+        n2 = float(input('Digite outro número: '))
+#        sleep(1)
+        print('Calculando...')
+#        sleep(2)
+        res = n1-n2
+        print('{:.2f} - {:.2f} = {:.2f}'.format(n1, n2, res))
+    elif opc == 3:
+        n1 = float(input('Digite um número: '))
+        n2 = float(input('Digite outro número: '))
+#        sleep(1)
+        print('Calculando...')
+#        sleep(2)
+        res = n1/n2
+        print('{:.2f} : {:.2f} = {:.2f}'.format(n1, n2, res))
+    elif opc == 4:
+        n1 = float(input('Digite um número: '))
+        n2 = float(input('Digite outro número: '))
+#        sleep(1)
+        print('Calculando...')
+#        sleep(2)
+        res = n1*n2
+        print('{:.2f} x {:.2f} = {:.2f}'.format(n1, n2, res))
+    elif opc == 5:
+        c = 0
+        cont = 'a'
+        n = int(input('Tabuada de qual número deseja saber? '))
+        while c != 11:
+            r = n * c
+            print('{} x {} = {}'.format(n, c, r))
+            c += 1
+            if c == 11:
+                while cont not in 'SsNn':
+                    cont = input('Deseja continuar [S/N]: ').upper()
+            if cont in 'Nn':
+                break
+        if cont in 'Nn':
+            break
